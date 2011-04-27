90655d3f92689e5f1cdc7fc0ed0f45befd85a00c
Avinasha Shastry <avinasha1987@gmail.com>
2011-04-28 03:20:41 +0530

    After devise migrate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vvce::Application.config.secret_token = '6d912da3029e67450254139878cd5f61bcab24ecf33173b487b6fcd43055bfaa8015191210033222694113765758dbe8a93b5ce696569ce39da0411cf6cc0324'
